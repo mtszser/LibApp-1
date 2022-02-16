45aa6ca93b3f3ebae90875b12d61188053c06654
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-02-16 18:03:46 +0100

    3-4 almost done

---
+++ Interfaces/IBookRepo.cs
@@ -0,0 +1,21 @@
+﻿using LibApp.Models;
+using System.Collections.Generic;
+
+namespace LibApp.Interfaces
+{
+    public interface IBookRepo
+    { 
+        IEnumerable<Book> GetBooks();
+        Book GetBookById(int id);
+
+        void AddBook(Book book);
+
+        void UpdateBook(Book book);
+
+        void DeleteBook(int id);
+
+        void Save();
+    }
+
+   
+}
